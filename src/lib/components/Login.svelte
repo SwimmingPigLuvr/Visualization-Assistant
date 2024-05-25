591c3718d2f00119a34a93f0600c4608325bb656
SwimmingPigLuvr <dnevell95@gmail.com>
2024-05-24 20:24:33 -0700

    sign in update

--- src/lib/components/Login.svelte
+++ src/lib/components/Login.svelte
@@ -1,32 +1,11 @@
 <script lang="ts">
-    import { auth, db } from "$lib/firebase";
+    import { auth } from "$lib/firebase";
     import { GoogleAuthProvider, signInWithPopup, signInWithRedirect, signOut } from "firebase/auth";
-    import { doc, getDoc, setDoc } from "firebase/firestore";
     import { SignedIn, SignedOut } from "sveltefire";
 
     async function signInWithGoogle() {
         const provider = new GoogleAuthProvider();
-        const userCredential = await signInWithPopup(auth, provider);
-        const user = userCredential.user;
-
-        // Check if the user document exists
-        const userDocRef = doc(db, "users", user.uid);
-        const userDoc = await getDoc(userDocRef);
-
-        // If the document does not exist, create it
-        if (!userDoc.exists()) {
-            const defaultData = {
-                accountType: "free",
-                photoURL: user.photoURL,
-                settings: {
-                    voiceID: "28VLndcKwmm1RxLQoOn"
-                },
-                threads: [],
-                username: user.displayName || "user" // Using display name or a default value
-            };
-
-            await setDoc(userDocRef, defaultData);
-        }
+        const user = await signInWithPopup(auth, provider);
     }
 
 </script>
@@ -38,5 +17,5 @@
 </SignedIn>
 
 <SignedOut>
-    <button class="p-2 px-4 bg-white bg-opacity-50 rounded-xl" on:click={signInWithGoogle}>sign in</button>
+    <button class="p-2 px-4 bg-white bg-opacity-50 rounded-xl" on:click={signInWithGoogle}>sign in here</button>
 </SignedOut>
