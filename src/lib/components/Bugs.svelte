97703e66ee8d6cdd3784f6f61fea2460c0fc39dd
remilioMilady <pootiequeenie@gmail.com>
2024-06-05 17:39:13 -0700

    Updated files: src/lib/components/Bugs.svelte

--- src/lib/components/Bugs.svelte
+++ src/lib/components/Bugs.svelte
@@ -1,7 +1,19 @@
-<script>
-
+<script lang="ts">
+    let reportBugWindowOpen = false;
 </script>
 
-<div>
-    <button></button>
+<div class="w-full p-2">
+    <button
+        on:click={() => (reportBugWindowOpen = true)}
+        class="animate-bounce text-3xl w-full text-right">🐛</button
+    >
 </div>
+
+{#if reportBugWindowOpen}
+    <div
+        class="z-50 font-sans -tracking-wide fixed -bottom-16 right-12 text-xl p-8 text-sky-300 bg-white rounded-xl rounded-br-none"
+    >
+        <p>please kindly message m3.</p>
+    </div>
+{/if}
+
