fcbeaaad5eddd50a04fb52f9768e9561684cad79
remilioMilady <pootiequeenie@gmail.com>
2024-06-22 15:09:06 -0700

    Updated files: src/lib/components/Menu.svelte

--- src/lib/components/Menu.svelte
+++ src/lib/components/Menu.svelte
@@ -65,12 +65,6 @@
 
     let voices: Voice[] = [
         {
-            id: "Atp5cNFg1Wj5gyKD7HWV",
-            name: "Natasha",
-            imageURL: "/images/voices/natasha.png",
-            audioPreviewURL: "/sounds/voice-previews/natasha.mp3",
-        },
-        {
             id: "7Uw4vgM4Qb1qiwwUnu15",
             name: "Sam",
             imageURL: "/images/voices/soothing-sam.png",
@@ -81,12 +75,6 @@
             name: "Matthew",
             imageURL: "/images/voices/materializePath.png",
             audioPreviewURL: "/sounds/voice-previews/matthew.mp3",
-        },
-        {
-            id: "aOcS60CY8CoaVaZfqqb5",
-            name: "Bleakoff",
-            imageURL: "/images/voices/bleakoffHandDox.png",
-            audioPreviewURL: "/sounds/voice-previews/bleakoff.mp3",
         },
         {
             id: "FVQMzxJGPUBtfz1Azdoy",
@@ -155,72 +143,72 @@
     }
 </script>
 
-    {#if $isMenuOpen}
-        <!-- bg overlay -->
-        <button
-            on:click={() => closeMenu()}
-            class="z-20 inset-10 h-screen w-screen fixed top-0 left-0"
-        ></button>
-
-        <!-- menu -->
-        <div
-            in:fly={{ x: -100, duration: 100, easing: cubicInOut }}
-            class="h-screen overflow-y-auto w-full sm:w-[390px] z-30 bg-blue-700 border-slate-500 border-[1px] bg-opacity-30 backdrop-blur relative top-0 p-2 flex flex-col space-y-2 items-start"
-        >
-            <!-- new visualization button -->
-            <button
-                on:mouseenter={() => (newVisualizationToolTip = true)}
-                on:mouseleave={() => (newVisualizationToolTip = false)}
-                on:click={() => handleCreateNewThread()}
-                class=" z-10 group absolute top-4 right-4 font-mono text-3xl font-black hover:text-white text-slate-400"
-                ><p
-                    class="transform transition-all duration-500 ease-in-out group-hover:scale-110"
-                >
-                    📝
-                </p>
-
-                {#if newVisualizationToolTip}
-                    <span
-                        in:fly={{ duration: 200, y: -10 }}
-                        class="text-sm absolute top-3 right-0 tooltip rounded shadow-lg px-2 p-1 bg-black border-white border-[1px] text-white mt-8"
-                        >new {$currentTechnique}</span
-                    >
-                {/if}
-            </button>
-
-            <!-- visualizations -->
-            <div class="pt-12 flex flex-col p-">
-                <Threads />
-            </div>
-
-            <Voices />
-
-            <div class="p-2 w-full m-auto left-0 absolute bottom-0">
-                <Bugs />
-            </div>
-        </div>
-    {/if}
+{#if $isMenuOpen}
+    <!-- bg overlay -->
     <button
-        on:mouseenter={() => (toggleMenuToolTip = true)}
-        on:mouseleave={() => (toggleMenuToolTip = false)}
-        on:click={() => toggleMenu()}
-        class="transform z-50 transition-all duration-500 ease-in-out group fixed top-4 left-4 font-mono text-3xl font-black hover:text-white text-slate-400"
-    >
-        <p class="group-hover:scale-110">
-            🌸
-            <!-- ✦ -->
-        </p>
+        on:click={() => closeMenu()}
+        class="z-20 inset-10 h-screen w-screen fixed top-0 left-0"
+    ></button>
 
-        {#if toggleMenuToolTip}
-            <span
-                in:fly={{ duration: 200, y: -10 }}
-                class="z-50 flex justify-center items-center text-sm absolute top-5 left-0 tooltip w-32 rounded shadow-lg px-2 p-1 bg-black border-white border-[1px] text-white mt-8"
+    <!-- menu -->
+    <div
+        in:fly={{ x: -100, duration: 100, easing: cubicInOut }}
+        class="h-screen overflow-y-auto w-full sm:w-[390px] z-30 bg-blue-700 border-slate-500 border-[1px] bg-opacity-30 backdrop-blur relative top-0 p-2 flex flex-col space-y-2 items-start"
+    >
+        <!-- new visualization button -->
+        <button
+            on:mouseenter={() => (newVisualizationToolTip = true)}
+            on:mouseleave={() => (newVisualizationToolTip = false)}
+            on:click={() => handleCreateNewThread()}
+            class=" z-10 group absolute top-4 right-4 font-mono text-3xl font-black hover:text-white text-slate-400"
+            ><p
+                class="transform transition-all duration-500 ease-in-out group-hover:scale-110"
             >
-                {#if $isMenuOpen}
-                    close menu
-                {:else}
-                    open menu
-                {/if}
-            </span>
-        {/if}
-    </button>
+                📝
+            </p>
+
+            {#if newVisualizationToolTip}
+                <span
+                    in:fly={{ duration: 200, y: -10 }}
+                    class="text-sm absolute top-3 right-0 tooltip rounded shadow-lg px-2 p-1 bg-black border-white border-[1px] text-white mt-8"
+                    >new {$currentTechnique}</span
+                >
+            {/if}
+        </button>
+
+        <!-- visualizations -->
+        <div class="pt-12 flex flex-col p-">
+            <Threads />
+        </div>
+
+        <Voices />
+
+        <div class="p-2 w-full m-auto left-0 absolute bottom-0">
+            <Bugs />
+        </div>
+    </div>
+{/if}
+<button
+    on:mouseenter={() => (toggleMenuToolTip = true)}
+    on:mouseleave={() => (toggleMenuToolTip = false)}
+    on:click={() => toggleMenu()}
+    class="transform z-50 transition-all duration-500 ease-in-out group fixed top-4 left-4 font-mono text-3xl font-black hover:text-white text-slate-400"
+>
+    <p class="group-hover:scale-110">
+        🌸
+        <!-- ✦ -->
+    </p>
+
+    {#if toggleMenuToolTip}
+        <span
+            in:fly={{ duration: 200, y: -10 }}
+            class="z-50 flex justify-center items-center text-sm absolute top-5 left-0 tooltip w-32 rounded shadow-lg px-2 p-1 bg-black border-white border-[1px] text-white mt-8"
+        >
+            {#if $isMenuOpen}
+                close menu
+            {:else}
+                open menu
+            {/if}
+        </span>
+    {/if}
+</button>
